3e6b80ea0bd4756ced560920025857d0c6403f96
Alex <alexcouts18@gmail.com>
2020-04-21 22:46:00 -0400

    initial set up completed with variables created and CSS reset implemented

---
+++ todo-client/src/App.js
@@ -0,0 +1,18 @@
+// * dependencies 
+import React from 'react';
+
+// * components:
+
+
+// * stylings:
+import './CSS/index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1> THIS IS THE HEADER </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-client/src/index.js
@@ -0,0 +1,16 @@
+// * dependencies: 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// * styling:
+
+// * components: 
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
